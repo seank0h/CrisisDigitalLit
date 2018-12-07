0ae9aa3c20ce0f52c1c34b45d6684d889d3ced16
Curtis Shea <cgs8601@rit.edu>
2018-12-07 16:03:08 -0500

    Made some notes in navigation for future code

--- js/navigation.js
+++ js/navigation.js
@@ -20,4 +20,12 @@ export function move(country){
     // 2. graph.getAdjacenciesByCountry(whateverCountryYouWant)
     
     // the adjcencies are numerical, but you can convert them to countries by graph.getCountry(id)
+    
 }
+
+// notes:
+// on click -> move(countryClicked).
+// reveal adjacent nodes and paths to the new country.
+// change the path used to a different color (to show path traveled).
+// possibly identify special effects depending on country (Nation X deports you to nation Y, one node changes routes for future node, end node resets?)
+// ^ All of those effects are pretty doable
