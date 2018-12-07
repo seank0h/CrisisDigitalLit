9c0cb219a2c3578fa51f115dda7f18884f3c9184
LeoHolman <lah2719@g.rit.edu>
2018-12-06 20:58:52 -0500

    Import d3, utilize for making nodes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+d3.select('#map-container')
+    .append("svg")
+    .attr('class','svg-icon')
+    .append('circle')
+    .attr('class','svg-icon')
+    .attr('r',50)
+    .attr('cx',50)    
+    .attr('cy',50)    
+    .attr('fill','red');
