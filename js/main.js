1b27eb5a22f5f98f43bf70d0d47fa0d9048a37a8
LeoHolman <lah2719@g.rit.edu>
2018-12-07 19:01:25 -0500

    Starting work to make nodes responsive

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@ import * as gtf from "./getTextFile.js";
 
 gtf.displayLog("the-netherlands");
 
-d3.select('#map-container')
+function drawNode() { //work toward passing in an object
+if (node) {
+    console.log("I'm here");
+}    // erase previous nodes on resize
+//check for nodes
+let nodeWidth = window.innerWidth * 0.005;
+let containerWidth = $('#map-container').width();
+let containerHeight = $('#map-container').height();
+var nodeX =  containerWidth / 20; //pass the object values here
+console.log(nodeX);
+var nodeY = containerHeight / 20;
+console.log(nodeY);
+var svg = d3.select('#map-container')
     .append("svg")
-    .attr('class','svg-icon')
+    .attr('viewbox','0 0' + containerWidth + '' + containerHeight)
+    .attr('preserveAspectRatio','xMidYMid meet')
+    .attr('id','svg');
+var node = svg    
     .append('circle')
     .attr('class','svg-icon')
-    .attr('r',50)
-    .attr('cx',50)    
-    .attr('cy',50)    
+    .attr('r',nodeWidth)
+    .attr('cx',nodeX)    
+    .attr('cy',nodeY)    
     .attr('fill','red');
+}
+window.addEventListener("resize",drawNode); //redraw the nodes on resize to ensure positioning correct
