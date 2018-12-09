55fb91863f21c0da4345b7e54ce0246a67b122f8
LeoHolman <lah2719@g.rit.edu>
2018-12-09 12:19:34 -0500

    Convert to Web API

---
+++ js/leafletFunctions.js
@@ -0,0 +1,57 @@
+var mymap = L.map('mapid')
+.setView([47.1625, 19.09], 5);
+
+
+var _lat;
+var _lng;
+var circle;
+
+function setLatLngFromCountryCode(_countryCode){
+    $.ajax({ url:'http://api.geonames.org/countryInfo?country=' +_countryCode + '&type=json&username=LeoHolman',
+             success: function(data){
+                saveLatLng(data.geonames[0].north,data.geonames[0].east);
+                circle = L.circle([data.geonames[0].north - ((data.geonames[0].north - data.geonames[0].south)/2), data.geonames[0].east - ((data.geonames[0].east - data.geonames[0].west)/2)] , {
+                    color: 'red',
+                    fillColor: '#f03',
+                    fillOpacity: 0.5,
+                    radius: 50000
+                }).addTo(mymap);
+             }
+    }) 
+}
+
+function saveLatLng(lat,lng){
+    _lat = lat;
+    _lng = lng;
+}
+
+function returnLatLng(){
+    return [_lat,_lng];
+}
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibGVvaG9sbWFuIiwiYSI6ImNqcGgxOHZhMjBlNGIzd3RlYmVmdmp0ZzEifQ.BoCaFJg56cfva3CHDIOsZw'
+}).addTo(mymap);
+
+var popup = L.popup();
+var country;
+mymap.on('click', function(e){
+    $.ajax({ url:'http://api.geonames.org/countryCode?lat='+e.latlng.lat+'&lng='+e.latlng.lng+'&type=json&username=LeoHolman',
+             success: function(data){
+                 country = data.countryName;
+                 $('.leaflet-popup-content').text(country);
+                 console.log("Lat"+ e.latlng.lat + "lng" + e.latlng.lng);
+                 console.log(data.countryName);
+             }
+    });
+   popup.setLatLng(e.latlng).setContent('').openOn(mymap);
+});
+
+
+
+setLatLngFromCountryCode("SY");
+setLatLngFromCountryCode("DE");
+setLatLngFromCountryCode("TR");
